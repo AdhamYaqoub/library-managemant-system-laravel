c71f0b206d0f9c976ed1b4729dbad5addffb0a31
AdhamYaqoub <amamry2021.2002@gmail.com>
2026-09-02 13:14:08 +0300

    Implement automatic member account creation

--- app/Http/Requests/StoreMemberRequest.php
+++ app/Http/Requests/StoreMemberRequest.php
@@ -23,7 +23,7 @@ class StoreMemberRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:255',
-            'email' => 'required|email|unique:members,email',
+            'email' => 'required|email|unique:users,email|unique:members,email',
         ];
     }
 }
